2b5e2092ae3000be22e287db3ff2a66f3b173786
SuperMT <18007092757@163.com>
2019-11-18 12:29:02 +0900

    finish experiments with multi thread workload, it takes too long to finish the running, so I cut down the workloads at 13 threads.

--- distribution_plotter.py
+++ distribution_plotter.py
@@ -9,5 +9,13 @@ for number in numbers:
     xs.append(int(10000 * float(number.replace("\n",""))))
 
 xs.sort()
-plt.plot(xs)
+
+values = set()
+
+for x in xs:
+    values.add(x)
+print(len(values))
+print(len(xs))
+
+plt.plot(list(values))
 plt.show()
