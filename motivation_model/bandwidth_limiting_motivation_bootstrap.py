f8742540b958a365b06749498236c60e44157f0e
supermt <supertempler@gmail.com>
2020-06-10 14:56:46 +0800

    add speed limiting in small scale dataset

--- motivation_model/bandwidth_limiting_motivation_bootstrap.py
+++ motivation_model/bandwidth_limiting_motivation_bootstrap.py
@@ -11,15 +11,15 @@ import os
 if __name__ == '__main__':
     env = HardwareEnvironment()
     CPU_IN_TOTAL = 12
-    io_bandwidth=[10,100,200,400]
+    io_bandwidth=[100,200,400]
     path_suffix = [str(band)+"mb" for band in io_bandwidth] 
     
     env.config_CPU_by_list([4,8])
-    env.config_Memory(min_mem=(64 * 1024 * 1024), set_size=1)
+    env.config_Memory(min_mem=(16 * 1024 * 1024), set_size=4)
 
-    env.add_storage_path("/home/jinghuan/rocksdb_hdd/rocksdb_test",StorageMaterial.SATAHDD)
-    env.add_storage_path("/home/jinghuan/rocksdb_sata/rocksdb_test",StorageMaterial.SATASSD)
-    env.add_storage_path("/home/jinghuan/rocksdb_nvme/rocksdb_test",StorageMaterial.NVMeSSD)
+    env.add_storage_path("/home/jinghuan/rocksdb_hdd",StorageMaterial.SATAHDD)
+    env.add_storage_path("/home/jinghuan/rocksdb_sata",StorageMaterial.SATASSD)
+    env.add_storage_path("/home/jinghuan/rocksdb_nvme",StorageMaterial.NVMeSSD)
     # env.add_storage_path("/home/jinghuan/rocksdb_pmem",StorageMaterial.NVMeSSD)
 
     reset_CPUs()
@@ -34,7 +34,7 @@ if __name__ == '__main__':
         os.system('cgset -r blkio.throttle.write_bps_device="259:0 '+str(bandwidth*1024*1024)+'" test_group1')
         # test for the limiting
         os.system('cat /sys/fs/cgroup/blkio/test_group1/blkio.throttle.write_bps_device')
-        DB_launcher(env,"/home/jinghuan/bandwidth_limiting/"+str(bandwidth)+"mb", db_bench= DEFAULT_DB_BENCH).run()
+        DB_launcher(env,"/home/jinghuan/smallset/bandwidth_limiting/"+str(bandwidth)+"mb", db_bench= DEFAULT_DB_BENCH).run()
 
     os.system("cgdelete blkio:/test_group1")
     reset_CPUs()
